715d3d7f2216d95954c43d81d324cc2da6669749
Rasim Aghayev <rasimaqayev@gmail.com>
2024-08-17 23:26:42 +0400

    fix: Task 2 complete. 1.3

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -3,8 +3,12 @@ from dataclasses import dataclass
 
 @dataclass
 class Operands:
-    first_operand: int
-    second_operand: int
+    def __init__(self, first_operand: int, second_operand: int):
+        self.first_operand = first_operand
+        self.second_operand = second_operand
+
+    def __repr__(self):
+        return f"Operands({self.first_operand}, {self.second_operand})"
 
 
 def operands_factory(first_operand: int, second_operand: int) -> Operands:
