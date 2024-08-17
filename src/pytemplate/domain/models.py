db0ba62fdf575b3c86f09110cfe4969ff9d8d839
Rasim Aghayev <rasimaqayev@gmail.com>
2024-08-17 23:59:01 +0400

    fix: Task 2 complete. 1.6

--- src/pytemplate/domain/models.py
+++ src/pytemplate/domain/models.py
@@ -6,9 +6,6 @@ class Operands:
     first_operand: int
     second_operand: int
 
-    def __repr__(self):
-        return f"Operands(first_operand={self.first_operand}, second_operand={self.second_operand})"
-
 
 def operands_factory(first_operand: int, second_operand: int) -> Operands:
     return Operands(first_operand, second_operand)
